04fa9eb8ea5cc56bb5f16768973a7405403ea1f9
GregLine <pyatov-nik@mail.ru>
2014-01-10 00:23:49 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import com.company.controllers.CinemaController;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CinemaController.getInstance().start();
+    }
+}
